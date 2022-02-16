f7ba537e082b2ebe8df9a17b2cf8fc502226cf86
XanGrab <sunbird.grabowski@gmail.com>
2022-02-16 13:01:28 -0600

    * broken prototype (will finish by Fri)

--- Match 3 Game Starter/Assets/Scripts/Board and Grid/BoardManager.cs
+++ Match 3 Game Starter/Assets/Scripts/Board and Grid/BoardManager.cs
@@ -3,11 +3,14 @@
  */
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour {
 	public static BoardManager instance;
+	private SpriteRenderer bg;
+
 	public GameObject tile;
 	public int xSize, ySize;
 
@@ -17,29 +20,43 @@ public class BoardManager : MonoBehaviour {
 
 	void Start () {
 		instance = GetComponent<BoardManager>();
+		bg = GetComponent<SpriteRenderer>();
 
 		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
         CreateBoard(offset.x, offset.y);
     }
-
+	
+	public void OnTouch(InputAction.CallbackContext ctx){
+		if(ctx.started)
+		{
+			Camera mainCam = Camera.main;
+			Vector2 pointerPos = Mouse.current.position.ReadValue();
+			pointerPos = mainCam.ScreenToWorldPoint(pointerPos);
+			RaycastHit2D hit = Physics2D.Raycast(pointerPos, Vector2.zero);
+			Debug.Log("Raycast @ " + pointerPos);
+			if (hit.collider != null) {
+				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+				StartCoroutine(ScaleMe(hit.transform));
+			}
+		}
+	}
+	IEnumerator ScaleMe(Transform objTr) {
+		objTr.localScale *= 1.2f;
+		yield return new WaitForSeconds(0.5f);
+		objTr.localScale /= 1.2f;
+	}
 	private void CreateBoard (float xOffset, float yOffset) {
 		tiles = new GameObject[xSize, ySize];
 
-        float startX = transform.position.x;
-		float startY = transform.position.y;
+        float startX = transform.position.x - (bg.bounds.extents.x * 0.8f);
+		float startY = transform.position.y - (bg.bounds.extents.y * 0.8f);
 		
 		int[] prevLeft = new int[ySize];
 		int prevBelow = -1;
 
 		for (int x = 0; x < xSize; x++) {
 			for (int y = 0; y < ySize; y++) {
-				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
-				tiles[x, y] = newTile;
-				newTile.transform.parent = transform;
-				
-				// Set the tile's color and ensure no starting march-3
-				Animator tileAnim = newTile.GetComponent<Tile>().animator;
-				int tileColor = newTile.GetComponent<Tile>().color;
+				int tileColor = 0;
 				
 				List<int> possibleColors = new List<int>(){0, 1, 2, 3, 4, 5};
 				
@@ -47,26 +64,12 @@ public class BoardManager : MonoBehaviour {
 				possibleColors.Remove(prevBelow);
 
 				tileColor = possibleColors[Random.Range(0, possibleColors.Count)];
-				switch(tileColor){
-					case 0:
-						tileAnim.Play("Grey");
-						break;
-					case 1:
-						tileAnim.Play("Blue");
-						break;
-					case 2:
-						tileAnim.Play("Green");
-						break;
-					case 3:
-						tileAnim.Play("Lime");
-						break;
-					case 4:
-						tileAnim.Play("Orange");
-						break;
-					case 5:
-						tileAnim.Play("Red");
-						break;
-				}
+				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
+				newTile.GetComponent<Tile>().color = tileColor;
+				Animator tileAnim = newTile.GetComponent<Tile>().animator;
+				tiles[x, y] = newTile;
+				newTile.transform.parent = transform;
+				
 				prevLeft[y] = tileColor;
 				prevBelow = tileColor;
 			}
--- Match 3 Game Starter/Assets/Scripts/Board and Grid/Tile.cs
+++ Match 3 Game Starter/Assets/Scripts/Board and Grid/Tile.cs
@@ -1,58 +1,70 @@
-﻿/*
+/*
  * Copyright (c) 2017 Razeware LLC - Jeff Fisher
  */
 
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour {
 	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
 	private static Tile previousSelected = null;
 
+	//[SerializeField]
 	private SpriteRenderer render;
 	public Animator animator;
 	public int color;
-	private bool isSelected = false;
 
-	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+	private bool isSelected = false;
 
 	void Awake() {
 		render = GetComponent<SpriteRenderer>();
 		animator = GetComponent<Animator>();
-    }
+	}
+
+	public void Start()
+	{
+		animator.SetFloat("Color", color);
+	}
 
 	public void OnTouch(InputAction.CallbackContext ctx){
-		if(ctx.started){
-			Debug.Log("I've been touched @:" + gameObject.transform.position);
-			/*if(render.sprite == null || BoardManager.instance.IsShifting){
-				return;
-			}
-
-			if(isSelected){
-				Deselect();
-			}else{
-				if(previousSelected == null){
-					Select();
-				}else{
-					previousSelected.Deselect();
-				}
-			}*/
+		if(ctx.started)
+		{
+			//Debug.Log("Touch logged @ " + );
 		}
 	}
 
+	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+
+	private void Touch()
+	{ 
+		Debug.Log("Tile(" + gameObject.transform.position + ") touched!");
+		if(BoardManager.instance.IsShifting){
+			return;
+		}
+
+		if(isSelected){
+			Deselect();
+		}else{
+			if(previousSelected == null){ Select();
+			}else{ previousSelected.Deselect(); }
+		}
+	}
 	private void Select() {
 		isSelected = true;
-		Debug.Log("I should get darker!");
-		render.color = selectedColor;
+		Debug.Log("I should be selected");
+		GetComponent<SpriteRenderer>().sharedMaterial.color = selectedColor;
 		previousSelected = gameObject.GetComponent<Tile>();
 	}
 
 	private void Deselect() {
 		isSelected = false;
-		render.color = Color.white;
+		Debug.Log("I should be deselected");
+		GetComponent<SpriteRenderer>().sharedMaterial.color = Color.white;
 		previousSelected = null;
 	}
-
 }
